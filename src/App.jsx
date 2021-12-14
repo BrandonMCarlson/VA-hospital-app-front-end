65965624301d94a6463eb1cc60d7ad5673e65b10
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-12-13 17:00:22 -0800

    working on goodle map API and functionality within my app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
+
+  const [user, setUser] = useState({});
+  const [allUsers, setAllUsers] = useState([]);
+  const [profile, setProfile] = useState({});
+  const [searchText, setSearchText] = useState("");
+
+  const render = localStorage.getItem("token");
+
+
+  const getAllUsers = async () => {
+    await axios
+      .get("http://localhost:5000/api/users")
+      .then((res) => {
+        setAllUsers(res.data);
+        console.log(res.data);
+      })
+  }
+  
+  const logoutUser = async () => {
+    console.log(localStorage.getItem("token"));
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/");
+    console.log(localStorage.getItem("token"));
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
