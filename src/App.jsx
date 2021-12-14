912875bb87bac9e2b4d1c03b4b97b0cf8a868c51
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-12-14 09:24:55 -0800

    add register page. add button component. add useForm.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MapContainer from "./components/MapContainer";
+import Register from "./pages/register";
 
 function App() {
   const [user, setUser] = useState({});
   const [allUsers, setAllUsers] = useState([]);
   const [profile, setProfile] = useState({});
   const [searchText, setSearchText] = useState("");
+  const [facility, setFacility] = useState({});
+  const [allFacilities, setAllFacilities] = useState({});
 
   const render = localStorage.getItem("token");
 
@@ -27,6 +30,17 @@ function App() {
     console.log(localStorage.getItem("token"));
   };
 
+  const getFacility = async () => {
+    await axios
+      .get(
+        "https://api.va.gov/services/community-care/v0/eligibility/openapi.json"
+      )
+      .then((res) => {
+        setAllFacilities(res.data);
+        console.log(res.data);
+      });
+  };
+
   useEffect(() => {
     getAllUsers();
   }, []);
@@ -35,13 +49,7 @@ function App() {
     <div className="App">
       <div>
         <MapContainer />
-        <div id="search_bar">
-          <div id="suggestions_anchor">
-            <input type="text" id="search_input" placeholder="Search for..." />
-          </div>
-        </div>
-        <div id="facet_results"></div>
-        <div id="search_results"></div>
+        <Register/>
       </div>
     </div>
   );
